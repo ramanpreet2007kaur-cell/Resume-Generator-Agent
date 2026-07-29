5d54a5d034da5d7e75a9a695a2a5acbb86cc4501
ramanpreet2007kaur-cell <ramanpreet2007kaur@gmail.com>
2026-07-29 14:35:58 +0530

    Update app (4).py

--- app (4).py
+++ app (4).py
@@ -11,24 +11,24 @@ import numpy as np
 from langchain.messages import SystemMessage, HumanMessage
 from langchain.agents import create_agent
 from PIL import Image
-import tempfile
 import base64
+import tempfile
 st.set_page_config(layout="wide")
 
 # =========================FRONTEND==================
 st.title("AI RESUME MAKER & JOB APPLY AGENT")
-st.image("https://chatgpt.com/s/m_6a687f2486008191b9d1691323fddba2")
-
+st.image("https://www.madrona.com/wp-content/uploads/2024/05/AI-Agent-infrastructure-blog-post-ChatGPT.webp",
+         width=400)
+# api keys
 GOOGLE_API_KEY = st.sidebar.text_input("Google Api Key", type = 'password')
 GROQ_API_KEY = st.sidebar.text_input("GROQ Api Key", type = 'password')
 TAVILY_API_KEY = st.sidebar.text_input("TAVILY Api Key", type = 'password')
 
-if not (GOOGLE_API_KEY) and not (GROQ) and not (TRAVILY):
-  st.warning("pass api key")
+if not (GOOGLE_API_KEY) and not(GROQ) and not (TAVILY):
+  st.sidebar.warning("pass api keys")
   st.stop()
-else :
+else:
   st.success("API KEYS LOADED")
-
 
 # ============= MODEL and AGENT CODE====================
 # tool 1
@@ -84,7 +84,6 @@ def prompt_generator():
 
 prompt_generator()
 
-
 # Final_Agent
 #Tool 2
 def prompt_reader():
@@ -92,47 +91,49 @@ def prompt_reader():
     prompt = f.read()
   return prompt
 
-prompt = """you are a helpful ai assistant  with a job resume maker , your task is to give html gormat 
-resume ,with a proper designing using recent html js css code , with professional degsine format , user
-will upload data and return html format resume make it diffrent colour scheme andthe resume should 
-project m skill set  also make it look like professional , create side margins table also make the text 
-gradient for heddings like professional summaryIMPORTANT: wherever the profile photo goes in the resume,
-output exactly this tag and nothing else:
+prompt = """you are a helpful ai assistant  with a job resume maker , your task is to give html format resume,
+with a proper designing using recent html js css code , with professional degsine format , user will upload data and
+return html format resume make it diffrent colour scheme andthe resume should project m skill set  also make it look
+like professional , create side margins table also make the text gradient for heddings like professional summary
+IMPORTANT: wherever the profile photo goes in the resume, output exactly this tag and nothing else:
 <img src="PROFILE_IMAGE_PLACEHOLDER" style="width:100px;height:100px;border-radius:50%;">
 do not draw or generate any other image tag or placeholder circle yourself"""
-#============================ UPLOAD IMAGE ================================
-#============================= IMAGE UPLOAD ==============================
 final_prompt = prompt + prompt_reader()
-FILE = st.sidebar.file_uploader(
-  "choose an image file",
-  type=["jpg","jpeg","png","webp"]
+
+#============IMAGE UPLOAD===========
+#===================UPLOAD IMAGE==============
+FILE= st.sidebar.file_uploader(
+   "Choose an image file",
+   type=["jpg","jpeg","png","webp"]
 )
-if FILE is not None :
-  try:
-    image=Image.open(FILE)
-    st.silebar.image(image,
-                captaim="Uploaded Image",
-                use_contained_width=True)
-    if image.mode in ("RGBA","P"):
-      image = image.convert("RGB")
+if FILE is not None:
+   try:
+      image=Image.open(FILE)
+      st.sidebar.image(image ,caption="Uploaded Image",use_contained_width=True)
 
-    base_name = os.path.splitext(FILE.name)[0]
-    save_path = f"{base_name}.jpg"
+      if image.mode in ("RGBA","P"):
+           image = image.convert("RGB")
 
-    image.save(save_path,"JPEG")
-    st.slidebar.success(f"🎉 Image Sucessfully saved as '{save_path}'")
+      base_name = os.path.splitext(FILE.name)[0]
+      save_path = f"{base_name}.jpg"
 
-  except Exception as e:
-    st.error(f"Error processing image:{e}")
-    
+      image.save(save_path,"JPEG")
+      st.sidebar.success(f" Image Successfully saved as '{save_path}'")
+      
+   except Exception as e:
+      st.error(f"Error processing image:{e}")
+
+profile_url = "https://s7d1.scene7.com/is/image/wbcollab/India_PM_Narendra_Modi-2?qlt=75&resMode=sharp2"
 
 # Change this when required new resume by user, pass details
+
 user_info = st.text_area("Give your information: ")
+
 
 user_query = f"""user details:given below:
 resume info {user_info}
-DEFAULT IF NOT GIVEN : PYTHON DEVELOPER RESUME"""
-
+DEFAULT IF NOT GIVEN  : PYTHON DEVELOPER RESUME"""
+    
 final_query = final_prompt + user_query
 
 OPTIONS = ["DELHI","NOIDA","GURGAON/GURUGRAM",
@@ -160,14 +161,14 @@ if st.button("Generate Resume"):
     response = agent.invoke({'messages':[{'role':'user',"content":final_query}]})
     code = response['messages'][-1].content[-1]['text']
 
-if FILE is not None :
-    with open(save_path,"rb") as image_file:
-             b64_image=base64.b64encode(img_file.read()).decode()
-    data_url=f"data:image/jpeg;base64,{b64_iamge}"
-    code=code.replace("PROFILE_IMAGE_PLACEHOLDER",data_uri)
+    if FILE is not None: 
+         with open(save_path,"rb") as image_file:
+                  b64_image=base64.b64encode(img_file.read()).decode()
+         data_uri=f"data:image/jpeg;base64,{b64_image}"
+         code=code.replace("PROFIL_IMAGE_PLACEHOLDER",data_uri)
 
-    st.html(code,width="stretch", unsafe_allow_jawascript=True)
-#=========================== APPLY LIVE JOBS ===========================
+    st.html(code, width="stretch", unsafe_allow_javascript=True)
+# =================APPLY LIVE JOBS============
     st.divider()
     response = agent.invoke({'messages':[{'role':'user',"content":job_prompt}]})
     job_code = response['messages'][-1].content[-1]['text']
